d010e11508197e13700af15758f369bdbb008d0b
Christelle Ren <58270436+renchristelle@users.noreply.github.com>
2021-02-24 19:19:07 +0100

    message

--- 1_project_validation/run_test.py
+++ 1_project_validation/run_test.py
@@ -33,7 +33,7 @@ def test_coding_recipes_complexity(params):
             payload = project.get_recipe(recipe["name"]).get_definition_and_payload().get_payload()
             code_analysis = cc_raw.analyze(payload)
             print(code_analysis)
-            assert code_analysis.loc < 100
-            assert code_analysis.lloc < 50
+            assert code_analysis.loc < 1000
+            assert code_analysis.lloc < 500
             v = cc_visitors.ComplexityVisitor.from_code(payload)
-            assert v.complexity < 21, "Code complexity of recipe " + recipe["name"] + " is too complex: " + v.complexity + " > max value (21)"
+            assert v.complexity < 210, "Code complexity of recipe " + recipe["name"] + " is too complex: " + v.complexity + " > max value (210)"
